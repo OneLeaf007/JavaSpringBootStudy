ea3f6eb41309f749014e6bf6b5f79306483835d1
liuhuabing <240303928@qq.com>
2020-09-16 04:15:15 +0800

    上传springBoot学习工程

---
+++ javaStudy/src/main/java/com/springboot/study/common/MybatiesConfig.java
@@ -0,0 +1,12 @@
+package com.springboot.study.common;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+//@MapperScan(value = "com.*")
+public class MybatiesConfig {
+
+
+}
---
+++ javaStudy/src/main/java/com/springboot/study/entiy/EntiyTest.java
@@ -0,0 +1,8 @@
+package com.springboot.study.entiy;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EntiyTest {
+
+}
